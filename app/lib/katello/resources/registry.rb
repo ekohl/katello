3367309c342aa5724d7489aca77b1e3eb5def8be
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2019-08-09 16:48:10 +0200

    Refs #27418 - Smart Proxy Pulp based registry URL
    This patch uses the Smart Proxy Pulp setting registry_url to determine
    where it can find the container registry. It assumes the same CA is used
    as the Smart Proxy itself rather than relying on the hard coded config.
    
    If there's a need for a different value, this could also be stored as a
    config and learned dynamically via a trusted channel that is the Proxy
    Registration.

--- app/lib/katello/resources/registry.rb
+++ app/lib/katello/resources/registry.rb
@@ -24,23 +24,24 @@ module Katello
           def load_class
             container_config = SETTINGS.dig(:katello, :container_image_registry)
             registry_url = nil
+            ca_cert_file = nil
 
-            if container_config
-              crane_url = container_config[:crane_url]
-              pulp_registry_url = container_config[:pulp_registry_url]
-              registry_url = crane_url if crane_url
-              # pulp 3 acts as its own registry
-              if pulp_registry_url && content_pulp3_support?(::Katello::DockerBlob::CONTENT_TYPE)
-                registry_url = pulp_registry_url
-              end
+            if content_pulp3_support?(::Katello::DockerBlob::CONTENT_TYPE)
+              registry_url = self.setting('Pulp3', 'registry_url')
+              # Assume the registry uses the same CA as the Smart Proxy
+              ca_cert_file = Setting[:ssl_ca_file]
+            elsif container_config
+              registry_url = container_config[:crane_url]
+              ca_cert_file = container_config[:crane_ca_cert_file]
             end
 
             logger.send(:error, "No container registry url specified") unless registry_url
+            logger.send(:warning, "No container ca cert file specified") unless ca_cert_file
 
             uri = URI.parse(registry_url)
             self.prefix = uri.path
             self.site = "#{uri.scheme}://#{uri.host}:#{uri.port}"
-            self.ca_cert_file = container_config[:registry_ca_cert_file]
+            self.ca_cert_file = ca_cert_file
             self
           end
 
